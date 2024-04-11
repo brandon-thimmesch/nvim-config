fa5c87f96158e1c05baac0a2f7add26599920f08
Brandon Thimmesch <brandonthimmesch@gmail.com>
2024-04-11 09:22:15 -0500

    Updated plugins and dashboard appearance

--- lua/plugins/autocomplete.lua
+++ lua/plugins/autocomplete.lua
@@ -5,8 +5,8 @@ return {
       table.insert(opts.sources, { name = "emoji" })
       local cmp = require("cmp")
       opts.mapping = vim.tbl_deep_extend("force", opts.mapping, {
-        ["<C-y>"] = cmp.mapping.confirm({ select = true }),
-        ["<CR>"] = cmp.config.disable,
+        "Tab"] = cmp.mapping.confirm({ select = true }),
+        ["CR"] = cmp.config.disable,
       })
     end,
   },
